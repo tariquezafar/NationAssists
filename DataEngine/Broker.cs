d037e4e7ada7357dee87d3ebaba2fe7ef000d8a4
tariquezafar <t.zafar007@gmail.com>
2021-06-01 09:24:59 +0530

    broker changes

--- DataEngine/Broker.cs
+++ DataEngine/Broker.cs
@@ -8,7 +8,7 @@ namespace DataEngine
 {
    public class Broker
     {
-     public string   BrokerId                       {get;set;}
+     public int   BrokerId                       {get;set;}
      public string   Broker_Type                    {get;set;}
      public string   Broker_Name                    {get;set;}
      public string   Office_Location_Address        {get;set;}
@@ -17,15 +17,15 @@ namespace DataEngine
      public string   Contact_Person_Contact_No      {get;set;}
      public string   Escalation_Person_Name         {get;set;}
      public string   Escalation_Person_Contact_No   {get;set;}
-     public string   Agreement_Start_Date           {get;set;}
-     public string   Agreement_End_Date             {get;set;}
-     public string   Commission_Paybable            {get;set;}
+     public DateTime   Agreement_Start_Date           {get;set;}
+     public DateTime Agreement_End_Date             {get;set;}
+     public decimal   Commission_Paybable            {get;set;}
      public string   Estimated_Business_In_A_Year   {get;set;}
      public string   Payment_Terms_Credit_Terms     {get;set;}
      public string   Remarks                        {get;set;}
      public string   Price_Option                   {get;set;}
-     public string   IsActive                       {get;set;}
-     public string   CreatedDate                    {get;set;}
+     public bool   IsActive                       {get;set;}
+     public DateTime   CreatedDate                    {get;set;}
      public string   MobileNo                       {get;set;}
      public string EmailId { get; set; }
 
@@ -35,11 +35,22 @@ namespace DataEngine
 
     public class BrokerDocument
     {
-      public int  BrokerDocumentId
-
-      public int  BrokerId
-      public int  DocumentPath
-      public int  CreatedDate
-      public int  IsActive
+      public int  BrokerDocumentId      {get;set;}
+                                        
+      public int  BrokerId              {get;set;}
+      public string  DocumentPath          {get;set;}
+      public DateTime  CreatedDate           {get;set;}
+      public bool IsActive { get; set; }
     }
+
+    public class BrokerOptedService
+    {
+     public int    BrokerServiceOptedId     {get;set;}
+     public int    BrokerId                 {get;set;}
+     public int    ServiceId                {get;set;}
+     public int    SubCategoryId            {get;set;}
+     public bool IsActive { get; set; }
+    }
+
+
 }
